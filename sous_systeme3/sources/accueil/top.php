bc996b86895731324b4dea99ea7b164e1fc0b0a6
andrealln <98532738+andrealln@users.noreply.github.com>
2024-05-27 09:35:44 +0200

    Add files via upload

---
+++ sous_systeme3/sources/accueil/top.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <body style="padding: 0;">
+    <div style="width: 100%; display: flex; justify-content: center;">
+        <div style="display: flex; flex-direction: column;">
+    <title>Tracking-BLE</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+       h2, h3, h5, p{
+            font-family: Arial, Helvetica, sans-serif;          
+        table, th, td {
+            border: 2px solid black;
+            border-collapse: collapse;
+            font-family: Arial, Helvetica, sans-serif; 
+        }
+        th, td {
+            padding: 5px;
+        }
+
+       button {
+            background-color: #1966B6;
+            color: white;
+            padding: 10px 20px;
+            font-size: 16px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 5px;
+        }
+
+        button:hover {
+            background-color: #000000;
+        }
+
+            .bouton_croix {
+             background: url("Images/croix.jpg");
+             width: 20px;
+             height: 20px;
+             background-repeat: no-repeat;
+             border: none;
+             cursor: pointer;
+           }
+       
+
+        .button-container {
+            display: flex;
+            justify-content: space-around;
+        }
+
+        .button-container div {
+            text-align: center; 
+        }
+
+    </style>
+</head>
